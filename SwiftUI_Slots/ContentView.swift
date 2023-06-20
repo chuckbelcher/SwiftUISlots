de738489254e66c0a2592171ef047a336c4ac127
Chuck Belcher <cfbelcher@gmail.com>
2023-06-20 17:15:13 -0400

    Added spin function to execute game play logic

--- SwiftUI_Slots/ContentView.swift
+++ SwiftUI_Slots/ContentView.swift
@@ -51,34 +51,7 @@ struct ContentView: View {
             HStack {
                 Spacer()
                 Button {
-                    if coins > 5 {
-                        print("Spinning")
-                        //Create array of images
-                        let images = ["cherry", "star", "apple"]
-                        
-                        //Create random numbers for images
-                        let slot1Rand = Int.random(in: 0...2)
-                        let slot2Rand = Int.random(in: 0...2)
-                        let slot3Rand = Int.random(in: 0...2)
-                        
-                        //Assign images
-                        slot1 = images[slot1Rand]
-                        slot2 = images[slot2Rand]
-                        slot3 = images[slot3Rand]
-                        
-                        //Check if winner and add or remove coins
-                        
-                        if slot1Rand == slot2Rand && slot1Rand == slot3Rand {
-                            print("winner winner chicken dinner")
-                            coins += 5
-                        } else {
-                            print("please try again")
-                            coins -= 5
-                        }
-                    } else {
-                        coins = 0
-                        status = "Game Over"
-                    }
+                    spin()
                 } label: {
                     Text("Spin")
                         .fontWeight(.bold)
@@ -108,7 +81,33 @@ struct ContentView: View {
     }
     
     func spin() {
-        
+        if coins > 5 {
+            //Create array of images
+            let images = ["cherry", "star", "apple"]
+            
+            //Create random numbers for images
+            let slot1Rand = Int.random(in: 0...2)
+            let slot2Rand = Int.random(in: 0...2)
+            let slot3Rand = Int.random(in: 0...2)
+            
+            //Assign images
+            slot1 = images[slot1Rand]
+            slot2 = images[slot2Rand]
+            slot3 = images[slot3Rand]
+            
+            //Check if winner and add or remove coins
+            
+            if slot1Rand == slot2Rand && slot1Rand == slot3Rand {
+                print("winner winner chicken dinner")
+                coins += 5
+            } else {
+                print("please try again")
+                coins -= 5
+            }
+        } else {
+            coins = 0
+            status = "Game Over"
+        }
     }
     
     func reset() {
