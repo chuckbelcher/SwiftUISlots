d0b8f7d706e743afe073ffc55d219e6592ff540a
Chuck Belcher <cfbelcher@gmail.com>
2023-06-19 20:49:39 -0400

    Initial Commit

---
+++ SwiftUI_Slots/SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI_Slots
+//
+//  Created by Chuck Belcher on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
